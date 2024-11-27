5dee817a9f80d7a0590db75765c2a2b60dee32c8
gn586 <gn586@HG22102004>
2024-11-27 10:53:03 +0700

    Add project files.

---
+++ Bài kiểm tra số 1/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Bài_kiểm_tra_số_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.txtd = new System.Windows.Forms.TextBox();
+            this.btt1 = new System.Windows.Forms.Button();
+            this.btt2 = new System.Windows.Forms.Button();
+            this.btt3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Họ tên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(43, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ngày sinh";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(43, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Giới tính";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(399, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 27);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Điểm toán";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(399, 113);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 27);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Điểm lý";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(399, 169);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 27);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Điểm hoá";
+            // 
+            // txt1
+            // 
+            this.txt1.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.Location = new System.Drawing.Point(159, 52);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(216, 30);
+            this.txt1.TabIndex = 8;
+            this.txt1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt1_KeyPress);
+            // 
+            // txt2
+            // 
+            this.txt2.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt2.Location = new System.Drawing.Point(159, 113);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(163, 31);
+            this.txt2.TabIndex = 9;
+            // 
+            // txt3
+            // 
+            this.txt3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt3.Location = new System.Drawing.Point(159, 179);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 34);
+            this.txt3.TabIndex = 10;
+            // 
+            // txt4
+            // 
+            this.txt4.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt4.Location = new System.Drawing.Point(292, 278);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(100, 34);
+            this.txt4.TabIndex = 11;
+            // 
+            // txta
+            // 
+            this.txta.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txta.Location = new System.Drawing.Point(523, 58);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(100, 31);
+            this.txta.TabIndex = 12;
+            this.txta.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress);
+            // 
+            // txtb
+            // 
+            this.txtb.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtb.Location = new System.Drawing.Point(523, 113);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(100, 31);
+            this.txtb.TabIndex = 13;
+            this.txtb.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress);
+            // 
+            // txtc
+            // 
+            this.txtc.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtc.Location = new System.Drawing.Point(523, 169);
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(100, 31);
+            this.txtc.TabIndex = 14;
+            this.txtc.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress);
+            // 
+            // txtd
+            // 
+            this.txtd.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtd.Location = new System.Drawing.Point(292, 342);
+            this.txtd.Name = "txtd";
+            this.txtd.Size = new System.Drawing.Size(100, 30);
+            this.txtd.TabIndex = 15;
+            // 
+            // btt1
+            // 
+            this.btt1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btt1.Location = new System.Drawing.Point(17, 278);
+            this.btt1.Name = "btt1";
+            this.btt1.Size = new System.Drawing.Size(269, 35);
+            this.btt1.TabIndex = 16;
+            this.btt1.Text = "Tình điểm trung bình";
+            this.btt1.UseVisualStyleBackColor = true;
+            this.btt1.Click += new System.EventHandler(this.btt1_Click);
+            // 
+            // btt2
+            // 
+            this.btt2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btt2.Location = new System.Drawing.Point(26, 333);
+            this.btt2.Name = "btt2";
+            this.btt2.Size = new System.Drawing.Size(114, 35);
+            this.btt2.TabIndex = 17;
+            this.btt2.Text = "Xếp loại";
+            this.btt2.UseVisualStyleBackColor = true;
+            this.btt2.Click += new System.EventHandler(this.btt2_Click);
+            // 
+            // btt3
+            // 
+            this.btt3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btt3.Location = new System.Drawing.Point(266, 423);
+            this.btt3.Name = "btt3";
+            this.btt3.Size = new System.Drawing.Size(126, 35);
+            this.btt3.TabIndex = 18;
+            this.btt3.Text = "Thoát";
+            this.btt3.UseVisualStyleBackColor = true;
+            this.btt3.Click += new System.EventHandler(this.btt3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 604);
+            this.Controls.Add(this.btt3);
+            this.Controls.Add(this.btt2);
+            this.Controls.Add(this.btt1);
+            this.Controls.Add(this.txtd);
+            this.Controls.Add(this.txtc);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Sinh viên";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.TextBox txtd;
+        private System.Windows.Forms.Button btt1;
+        private System.Windows.Forms.Button btt2;
+        private System.Windows.Forms.Button btt3;
+    }
+}
+
---
+++ Bài kiểm tra số 1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bài_kiểm_tra_số_1
+{
+    public partial class Form1 : Form
+    {
+        public double d = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btt3_Click(object sender, EventArgs e)
+        {
+            DialogResult thongbao = (MessageBox.Show("Bạn có muốn thoát không ?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question));
+            if (thongbao == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+
+
+        private void btt2_Click(object sender, EventArgs e)
+        {
+
+            if (d <= 5.0)
+            {
+                txtd.Text = "Trung Bình";
+            }
+            else if (d >= 5.0 && d <= 7.0)
+            {
+                txtd.Text = "Khá";
+            }
+            else
+            {
+                txtd.Text = "Giỏi";
+            }
+        }
+
+       
+
+        private void btt1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(txta.Text);
+                double b = double.Parse(txtb.Text);
+                double c = double.Parse(txtc.Text);
+
+                 d = (a + b + c) / 3;
+
+                txt4.Text = d.ToString();
+            }
+            catch 
+            {
+                MessageBox.Show("Vui lòng nhập lại số", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txt1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(Char.IsDigit(e.KeyChar)&&!Char.IsControl(e.KeyChar)) {
+                e.Handled = true;
+            }
+        }
+        private void txt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(!Char.IsDigit(e.KeyChar)&&!Char.IsControl(e.KeyChar)&&e.KeyChar!='.') { 
+                e.Handled = true;
+            }
+            if(e.KeyChar=='.'&&(sender as TextBox).Text.IndexOf(".")>=0) {
+                e.Handled=true;
+            }
+        }
+    }
+}
